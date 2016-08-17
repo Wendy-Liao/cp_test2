0630ffbaef12cb7c38acd044f54da1b98ea8aed5
WendyLiao <Wendy.Liao@quantatw.com>
2016-08-17 10:10:47 +0800

    [firmware_version.py]
    add firmware_version test

---
+++ firmware_version.py
@@ -0,0 +1,22 @@
+from PyQt5.QtCore import pyqtSignal
+from PyQt5.QtCore import QThread
+import subprocess
+
+
+class FirmwareVersion(QThread):
+    sig_update_ui = pyqtSignal(object)
+
+    def __init__(self):
+        QThread.__init__(self)
+        self.cmd = 'adb shell ./home/flex/bin/fct1-main.sh  FCT.1.8.2'
+
+    def run(self):
+        self.sig_update_ui.emit('FirmwareVersion test')
+        result = subprocess.Popen(self.cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
+        (output, err) = result.communicate()
+        return_code = result.wait()
+        if return_code == 0:
+            self.sig_update_ui.emit(str(output, encoding='UTF-8'))
+        else:
+            self.sig_update_ui.emit(str(err, encoding='UTF-8'))
+        self.sig_update_ui.emit('FirmwareVersion end')
