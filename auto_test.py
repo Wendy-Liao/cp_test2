572209c753349265bedddde423821237a4ff383a
WendyLiao <Wendy.Liao@quantatw.com>
2016-09-01 16:08:27 +0800

    [auto_test.py]
    change under_autotesting to under_auto_testing

--- auto_test.py
+++ auto_test.py
@@ -1,6 +1,7 @@
 from PyQt5.QtCore import pyqtSignal
 from PyQt5.QtCore import QThread
 
+
 class AutoTest(QThread):
     sig_update_ui = pyqtSignal(object)
 
@@ -11,7 +12,7 @@ class AutoTest(QThread):
 
     def run(self):
         self.sig_update_ui.emit('AutoTest')
-        self.main_ui.under_autotesting = True
+        self.main_ui.under_auto_testing = True
         while not self.main_ui.q.empty():
             self.sig_update_ui.emit('='*20)
             self.auto_running_task = self.main_ui.q.get()
@@ -19,4 +20,4 @@ class AutoTest(QThread):
             self.auto_running_task.wait()
 
         self.sig_update_ui.emit('AutoTest end')
-        self.main_ui.under_autotesting = False
+        self.main_ui.under_auto_testing = False
