c303740e8ef599bfb22e09b6e0c00f66e9738ead
WendyLiao <Wendy.Liao@quantatw.com>
2016-09-01 16:27:25 +0800

    [nand.py]
    1.add update_btn_status('item_name', 'Pass/Fail')
    2.add log_to_file('item_name', 'Pass/Fail', output)

--- nand.py
+++ nand.py
@@ -9,17 +9,27 @@ class Nand(QThread):
     def __init__(self, main_ui):
         QThread.__init__(self)
         self.main_ui = main_ui
-        self.cmd = 'adb shell ./home/flex/bin/fct1-main.sh  FCT.1.2.1 '
+        self.cmd = 'adb shell ./home/flex/bin/fct1-main.sh  FCT.1.6.1 2 '
 
     def run(self):
-        self.sig_update_ui.emit('Nand test')
         self.main_ui.under_testing = True
-        result = subprocess.Popen(self.cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
+        self.sig_update_ui.emit('Nand test')
+        result = subprocess.Popen(self.cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True,
+                                  universal_newlines=True)
         (output, err) = result.communicate()
         return_code = result.wait()
+
         if return_code == 0:
-            self.sig_update_ui.emit(str(output, encoding='UTF-8'))
+            self.sig_update_ui.emit(output)
+            if 'PASS' in output:
+                self.main_ui.update_btn_status('NAND', 'Pass')
+                self.main_ui.log_to_file('NAND', 'Pass', output)
+            else:
+                self.main_ui.update_btn_status('NAND', 'Fail')
+                self.main_ui.log_to_file('NAND', 'Fail', output)
         else:
-            self.sig_update_ui.emit(str(err, encoding='UTF-8'))
+            self.sig_update_ui.emit(err)
+            self.main_ui.update_btn_status('NAND', 'Fail')
+            self.main_ui.log_to_file('NAND', 'Fail', err)
         self.sig_update_ui.emit('Nand end')
         self.main_ui.under_testing = False
